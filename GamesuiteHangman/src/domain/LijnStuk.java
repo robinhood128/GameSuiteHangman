ce1d549a57a0d29d95bf53f493ad5a12964f6d56
robinhood128 <robbedecraemer@hotmail.com>
2017-05-09 11:51:02 +0200

    Lijnstuk aanpassingen Robbe & Helder

--- GamesuiteHangman/src/domain/LijnStuk.java
+++ GamesuiteHangman/src/domain/LijnStuk.java
@@ -2,8 +2,8 @@ package domain;
 
 public class Lijnstuk extends Vorm{
 	
-	private Punt startPunt;
-	private Punt eindPunt;
+	private Punt startHoekPunt;
+	private Punt eindHoekPunt;
 	
 	public Lijnstuk(Punt startPunt, Punt eindPunt) {
 		
@@ -18,17 +18,17 @@ public class Lijnstuk extends Vorm{
 		} else if (startPunt.equals(eindPunt)) {
 			throw new DomainException("Punten mogen niet gelijk zijn aan elkaar");
 		} else {
-		this.startPunt = startPunt;
-		this.eindPunt = eindPunt;
+		this.startHoekPunt = startPunt;
+		this.eindHoekPunt = eindPunt;
 		}
 	}	
 
 	public Punt getStartPunt() {
-		return startPunt;
+		return startHoekPunt;
 	}
 
 	public Punt getEindPunt() {
-		return eindPunt;
+		return eindHoekPunt;
 	}	
 	
 	public boolean equals(Object object) {
@@ -37,13 +37,13 @@ public class Lijnstuk extends Vorm{
 			return false;
 		} else {
 			Lijnstuk o = (Lijnstuk)object;
-			return o.startPunt.equals(startPunt) && o.eindPunt.equals(eindPunt);
+			return o.startHoekPunt.equals(startHoekPunt) && o.eindHoekPunt.equals(eindHoekPunt);
 		}
 	}
 	
 	@Override
 	public String toString() {
-		return "Lijn: startpunt: " + startPunt.toString() + " - eindpunt: " + eindPunt.toString();
+		return "Lijn: startpunt: " + startHoekPunt.toString() + " - eindpunt: " + eindHoekPunt.toString();
 	}
 	
 }
