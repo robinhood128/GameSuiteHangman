28c184fc252c655119576190a79f07bfb7df27e7
robinhood128 <robbedecraemer@hotmail.com>
2017-05-09 11:08:49 +0200

    Driehoek opgelost Robbe & Helder

--- GamesuiteHangman/src/domain/Driehoek.java
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -2,10 +2,10 @@ package domain;
 
 public class Driehoek extends Vorm
 {
-	private Punt punt1;
-	private Punt punt2;
-	private Punt punt3;
-	
+	private Punt hoekPunt1;
+	private Punt hoekPunt2;
+	private Punt hoekPunt3;
+
 	public Driehoek(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3)
 	{
 		if (hoekPunt1 == null)
@@ -18,33 +18,33 @@ public class Driehoek extends Vorm
 			throw new DomainException("Punten mogen niet samen vallen.");
 		if ((hoekPunt2.getX() - hoekPunt1.getX()) * (hoekPunt3.getY() - hoekPunt1.getY()) == (hoekPunt3.getX() - hoekPunt1.getX()) * (hoekPunt2.getY() - hoekPunt1.getY()))
 			throw new DomainException("Punten mogen niet op een lijn liggen.");
-		setPunt1(hoekPunt1);
-		setPunt2(hoekPunt2);
-		setPunt3(hoekPunt3);
+		setHoekPunt1(hoekPunt1);
+		setHoekPunt2(hoekPunt2);
+		setHoekPunt3(hoekPunt3);
+	}
+	
+	public Punt getHoekPunt1() {
+		return hoekPunt1;
 	}
 
-	public Punt getPunt1() {
-		return punt1;
+	public void setHoekPunt1(Punt hoekPunt1) {
+		this.hoekPunt1 = hoekPunt1;
 	}
 
-	public void setPunt1(Punt punt1) {
-		this.punt1 = punt1;
+	public Punt getHoekPunt2() {
+		return hoekPunt2;
 	}
 
-	public Punt getPunt2() {
-		return punt2;
+	public void setHoekPunt2(Punt hoekPunt2) {
+		this.hoekPunt2 = hoekPunt2;
 	}
 
-	public void setPunt2(Punt punt2) {
-		this.punt2 = punt2;
+	public Punt getHoekPunt3() {
+		return hoekPunt3;
 	}
 
-	public Punt getPunt3() {
-		return punt3;
-	}
-
-	public void setPunt3(Punt punt3) {
-		this.punt3 = punt3;
+	public void setHoekPunt3(Punt hoekPunt3) {
+		this.hoekPunt3 = hoekPunt3;
 	}
 	
 	public boolean equals(Object object)
@@ -52,11 +52,11 @@ public class Driehoek extends Vorm
 		if(!(object instanceof Driehoek))
 			return false;
 		Driehoek o = (Driehoek)object;
-		return o.punt1.equals(punt1) && o.punt2.equals(punt2) && o.punt3.equals(punt3);
+		return o.hoekPunt1.equals(hoekPunt1) && o.hoekPunt2.equals(hoekPunt2) && o.hoekPunt3.equals(hoekPunt3);
 	}
 	
 	public String toString()
 	{
-		return "Driehoek: hoekpunt1: " + punt1.toString() + " hoekpunt2: " + punt2.toString() + " hoekpunt3: " + punt3.toString();
+		return "Driehoek: hoekpunt1: " + hoekPunt1.toString() + " hoekpunt2: " + hoekPunt2.toString() + " hoekpunt3: " + hoekPunt3.toString();
 	}
 }
