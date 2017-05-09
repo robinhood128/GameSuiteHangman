408072ffc8706ef204608d94bdede938849df06d
LAPTOP-Q3476AF6\brech <brecht.merens@hotmail.com>
2017-05-09 10:59:47 +0200

    Denker Brecht en Doener Ruben: Klasse Omhullende gemaakt nog niet af

---
+++ GamesuiteHangman/src/domain/Omhullende.java
@@ -0,0 +1,56 @@
+package domain;
+
+public class Omhullende {
+	
+	private Punt positieLinksBoven;
+	private int breedte, hoogte;
+	
+	public Omhullende(Punt positieLinksBoven, int breedte, int hoogte) {
+		
+		setLinkerBovenhoek(positieLinksBoven);
+		setBreedte(breedte);
+		setHoogte(hoogte);
+		
+	}
+
+	public Punt getLinkerBovenhoek() {
+		return positieLinksBoven;
+	}
+
+	public void setLinkerBovenhoek(Punt positieLinksBoven) {
+		if (positieLinksBoven == null) {
+			throw new DomainException("Punt mag niet null zijn");
+		} 
+		this.positieLinksBoven = positieLinksBoven;
+	}
+
+	public int getBreedte() {
+		return breedte;
+	}
+
+	public void setBreedte(int breedte) {
+		if (breedte < 0) {
+			throw new DomainException("Breedte moet groter of gelijk aan nul zijn.");
+		}
+		this.breedte = breedte;
+	}
+
+	public int getHoogte() {
+		return hoogte;
+	}
+
+	public void setHoogte(int hoogte) {
+		if(hoogte <0 ) {
+			throw new DomainException("Hoogte moet groter of gelijk aan nul zijn");
+		}
+		this.hoogte = hoogte;
+	}
+	
+	public boolean equals(Object object) {
+		return false;
+	}
+	
+	public String toString() {
+		return "Omhullende: (" + getLinkerBovenhoek().getX() + ", " + getLinkerBovenhoek().getY() + ") - " + getBreedte() + " - " + getHoogte();
+	}
+}
--- GamesuiteHangman/src/domain/Rechthoek.java
+++ GamesuiteHangman/src/domain/Rechthoek.java
@@ -49,6 +49,10 @@ public class Rechthoek
 		this.hoogte = hoogte;
 	}
 	
+	public String getOmhullende() {
+		return toString() + "\n" + getOmhullende();
+	}
+	
 	public boolean equals(Object object)
 	{
 		if(!(object instanceof Rechthoek))
